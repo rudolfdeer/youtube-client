3b9db865a4e2cc5094ded0147cb62bb5a6097800
rudolfdeer <tomalichko@gmail.com>
2022-02-17 19:13:58 +0300

    feat: add pagination

--- youtube-client/src/components/Page.tsx
+++ youtube-client/src/components/Page.tsx
@@ -1,12 +1,13 @@
 import { Grid } from '@mui/material';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import VideoCard from './Card';
 import SearchInput from './Input';
 import styled from 'styled-components';
-import { useMutation, useQuery } from 'react-query';
+import { useQuery } from 'react-query';
 import youtubeApi from '../constants/api';
 import { Video } from '../interfaces';
+import ReactPaginate from 'react-paginate';
 
 const Main = styled.main`
   max-width: 1200px;
@@ -17,8 +18,25 @@ const Main = styled.main`
   align-items: center;
 `;
 
+
 export default function Page(): JSX.Element {
   const [value, setValue] = useState('');
+  const [offset, setOffset] = useState(4);
+  const [perPage] = useState(4);
+  const [pageCount, setPageCount] = useState(1);
+  const [cards, setCards] = useState([]);
+  const [videos, setVideos] = useState([]);
+
+  const { isLoading, error, data, refetch } = useQuery<Video[], Error>(
+    'videos',
+    () => handleSubmit(),
+    { enabled: false }
+  );
+
+  useEffect(() => {
+    setPageCount(Math.ceil(videos.length / perPage));
+    setCards(videos.slice(offset - perPage, offset));
+  }, [offset, videos]);
 
   const handleSubmit = async () => {
     if (!value) return;
@@ -31,14 +49,18 @@ export default function Page(): JSX.Element {
     });
 
     refetch();
+    setVideos(response.data.items);
     return response.data.items;
   };
 
-  const { isLoading, error, data, refetch } = useQuery<Video[], Error>(
-    'videos',
-    () => handleSubmit(),
-    { enabled: false }
-  );
+  const handlePageClick = (e: {
+    selected: number;
+  }) => {
+    const selectedPage = e.selected;
+    setOffset((selectedPage + 1) * perPage);
+  };
+
+  
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -51,8 +73,8 @@ export default function Page(): JSX.Element {
         value={value}
         setValue={setValue}
       />
-      <Grid container spacing={2}>
-        {data?.map((el: Video) => {
+      <Grid container spacing={2} style={{marginBottom: '20px'}}>
+        {cards?.map((el: Video) => {
           return (
             <Grid item xs={3} key={el.id.videoId}>
               <VideoCard video={el} />
@@ -60,6 +82,18 @@ export default function Page(): JSX.Element {
           );
         })}
       </Grid>
+      <ReactPaginate
+              previousLabel={'prev'}
+              nextLabel={'next'}
+              breakLabel={'...'}
+              breakClassName={'break-me'}
+              pageCount={pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={handlePageClick}
+              containerClassName={'pagination'}
+              activeClassName={'active'}
+            />
     </Main>
   );
 }
